abe6451235c989dcf1cc14c6cc6263f4950e982d
Gautam Bisht <bishtgautam012@gmail.com>
2024-01-23 13:22:42 +0530

    signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config({ path: ".env" });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const app = express();
-
-app.listen(3000, (req, res) => {
-  res(hello);
-  console.log("Server is running on port 3000");
+app.use(express.json());
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
