3b1a44546b218a3edeb235dfd526915a9cdb5338
Gautam Bisht <bishtgautam012@gmail.com>
2024-08-20 11:26:44 +0530

    Update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <>
       <Navbar className="border-b-2">
@@ -33,11 +35,34 @@ const Header = () => {
           <Button className="w-12 h-10 hidden sm:inline" pill color="gray">
             <FaMoon />
           </Button>
-          <Link to="/sign-in">
-            <Button gradientDuoTone="purpleToBlue" outline>
-              Sign In
-            </Button>
-          </Link>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar alt="user" rounded img={currentUser.profilePicture} />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">@{currentUser.username}</span>
+                <span className="block text-sm font-bold truncate">
+                  {currentUser.email}
+                </span>
+              </Dropdown.Header>
+              <Link to={"/dashboard?tab=profile"}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to="/sign-in">
+              <Button gradientDuoTone="purpleToBlue" outline>
+                Sign In
+              </Button>
+            </Link>
+          )}
+
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
